cd2c88660df9072181dc548eeb5bb0c6b29de96a
yg1110 <younggil9488@gmail.com>
2024-09-30 22:31:29 +0900

    eslint, prettierr 설정 변경

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,12 @@
 module.exports = {
   root: true,
   extends: '@react-native',
+  plugins: ['react', 'simple-import-sort'],
+  rules: {
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': 'warn',
+    'react-hooks/exhaustive-deps': 'warn',
+    'simple-import-sort/imports': 'error',
+    'simple-import-sort/exports': 'error',
+  },
 };
--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,5 @@
 module.exports = {
-  arrowParens: 'avoid',
-  bracketSameLine: true,
-  bracketSpacing: false,
+  printWidth: 120,
   singleQuote: true,
-  trailingComma: 'all',
+  tabWidth: 2,
 };
